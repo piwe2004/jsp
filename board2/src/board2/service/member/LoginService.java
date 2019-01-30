703c677904ba50e80789115f5002bffcfa95d282
piwe2004 <38637510+piwe2004@users.noreply.github.com>
2019-01-30 10:44:33 +0900

    2019/01/30

--- board2/src/board2/service/member/LoginService.java
+++ board2/src/board2/service/member/LoginService.java
@@ -1,16 +1,16 @@
-package board2.service;
+package board2.service.member;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import board2.controller.CommonAction;
 
-public class terms implements CommonAction{
+public class LoginService implements CommonAction{
 	
 	@Override
 	public String requestProc(HttpServletRequest req, HttpServletResponse resp) {
 		
-		return "/terms.jsp";
+		return "/login.jsp";
 		
 	}
 	
--- board2/src/board2/service/member/RegisterService.java
+++ board2/src/board2/service/member/RegisterService.java
@@ -1,16 +1,16 @@
-package board2.service;
+package board2.service.member;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import board2.controller.CommonAction;
 
-public class Login implements CommonAction{
+public class RegisterService implements CommonAction{
 	
 	@Override
 	public String requestProc(HttpServletRequest req, HttpServletResponse resp) {
 		
-		return "/login.jsp";
+		return "/register.jsp";
 		
 	}
 	
